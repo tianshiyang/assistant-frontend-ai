8bae0ddc97bba6e36ae5e1709f9fb23aeddc836d
田世洋 <tianshiyang@icourt.cc>
2026-01-16 23:23:52 +0800

    feat: 安装项目用到的项目依赖

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      dts: true, // 生成类型声明文件
+    }),
+    Components({
+      resolvers: [
+        AntDesignVueResolver({
+          importStyle: false
+        }),
+      ],
+    })
+  ],
 })
